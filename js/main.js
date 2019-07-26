14f9bf80acd7c39eb2be6c5f9e33d01efae13bea
Daniil Drobot <funplacebo7@gmail.com>
2019-07-26 21:49:30 +0300

    coming-body is done

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $('.slickslider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 2,
+        // autoplay: true,
+        draggable: true,
+        // dots: false,
+        prevArrow: false,
+        nextArrow: false,
+        autoplaySpeed: 3000
+    });
+});
