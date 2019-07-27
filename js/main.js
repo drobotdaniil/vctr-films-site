7f189df77b9c0bebc27719cd260e6cb7fdf0dc52
Daniil Drobot <funplacebo7@gmail.com>
2019-07-27 11:50:49 +0300

    added slider to popular block

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
 $(document).ready(function () {
-    $('.slickslider').slick({
+    $('.slickslider-coming').slick({
         slidesToShow: 4,
         slidesToScroll: 2,
         // autoplay: true,
+        draggable: true,
+        // dots: false,
+        prevArrow: false,
+        nextArrow: false,
+        autoplaySpeed: 3000
+    });
+    $('.slickslider-popular').slick({
+        slidesToShow: 3,
+        slidesToScroll: 2,
+        // autoplay: true,
+        vertical: true,
+        verticalSwiping: true,
         draggable: true,
         // dots: false,
         prevArrow: false,
