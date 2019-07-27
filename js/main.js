e949f499507504ff65468addc3a103d6fd89ef00
Daniil Drobot <funplacebo7@gmail.com>
2019-07-27 16:10:41 +0300

    testing

--- js/main.js
+++ js/main.js
@@ -16,9 +16,37 @@ $(document).ready(function () {
         vertical: true,
         verticalSwiping: true,
         draggable: true,
+        infinite: false,
         // dots: false,
         // prevArrow: false,
         // nextArrow: false,
         autoplaySpeed: 3000
     });
+});
+const slider = document.querySelector('.slider');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', (e) => {
+    isDown = true;
+    slider.classList.add('act');
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+});
+slider.addEventListener('mouseleave', () => {
+    isDown = false;
+    slider.classList.remove('act');
+});
+slider.addEventListener('mouseup', () => {
+    isDown = false;
+    slider.classList.remove('act');
+});
+slider.addEventListener('mousemove', (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const walk = (x - startX) * 1.5; //scroll-fast
+    slider.scrollLeft = scrollLeft - walk;
+    console.log(walk);
 });
